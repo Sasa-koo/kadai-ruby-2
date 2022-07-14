9d3dc5e3c5d692a74d9501cbcb26cf23b7f8dbaa
Sasa.koo <66.sasasa.k@gmail.com>
2022-07-14 13:07:43 +0000

    second commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,16 @@
   num_max = 100
   def fizzBuzz(num)
     if num % 3 == 0 && num % 5 == 0
-      puts "FizzBuzz"      
+    	"FizzBuzz"      
     elsif num % 3 == 0
-      puts "Fizz"
+    	"Fizz"
     elsif num % 5 == 0
-      puts "Buzz"
+    	"Buzz"
     else
-      puts num
+      num
     end
   end
   
   (1..num_max).each do |num|
-  	fizzBuzz(num)
+  	puts fizzBuzz(num)
   end
